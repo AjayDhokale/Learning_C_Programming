e5ec0064872557ba7b64b7a825eac3395e2dae03
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2025-01-28 17:14:31 +0530

    if else in C

---
+++ 05_If_Else.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+int main() {
+    // int i=10;
+    // int j=20;
+
+    // if( i < j ) {
+    //     i=30;                                               // If Statement
+    // }
+    
+    // printf("i=%d j=%d", i, j);
+    
+    // --------------------------------------------------------------------------------------------------------------
+    
+    // int i=40;
+    // int j=20;
+
+    // if( i < j   ) {                                            // If-else Statement
+    //     i=50;
+    // }
+    // else {
+    //     i=0;
+    // }
+    
+    // printf("\n\ni=%d j=%d\n\n", i, j);
+
+    // --------------------------------------------------------------------------------------------------------------
+
+
+
+    // int i=10;
+    // int j=20;
+    // int k=30;
+
+    // if( i == j ) {                                            // If-else Ladder Statement
+    //     i=30;
+    // }
+    // else if( i == k ) {
+    //     i=40;
+    // }
+    // else if( i > j ) {
+    //     i=50;
+    // }
+    // else if( i > k ) {
+    //     i=60;
+    // }
+    // else {
+    //     i = 0;
+    // }
+    
+    // printf("\n\ni=%d j=%d k=%d\n\n", i, j, k);
+
+
+    // ------------------------------------------------------    Even Odd      --------------------------------------------------------
+
+    // int num;
+
+    // printf("Enter The value of  Num: ");
+    // scanf("%d", &num);
+
+    // if( num%2 == 0 ) {
+    //     printf("The given number %d is even", num);
+    // }
+    // else {
+    //     printf("The given number %d is odd", num);
+    // }
+
+
+    // ------------------------------------------------------    Prime Number       --------------------------------------------------------
+
+    // int num,i;
+
+    // printf("Enter The Value Of Num : ");
+    // scanf("%d", &num);
+
+    // for( i=0; i <= num/2; ++i){
+    //     if( num%i == 0 ){
+    //         printf("%d is not a prime number.", num);
+
+    //         break;
+    //     } else {
+    //         printf("%d is a prime number.", num);
+        
+    //     }
+
+    // }    
+
+
+    //    int num,i, flag=0;
+
+    // printf("Enter The Value Of Num : ");
+    // scanf("%d", &num);
+
+    // for( i=2; i <= num/2; ++i){
+    //     if( num%i == 0 ){
+    //         flag =1;
+    //         break;
+    //     }
+
+    // }    
+    // if (flag == 0){
+    //      printf("%d is a prime number.", num);
+    // }else {
+    //      printf("%d is not a prime number.", num);
+    // }
+
+
+   return 0;
+
+}
+
